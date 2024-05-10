31ff3abd812b53e4e70ee898817ec69a9e942eca
crmulder <conrad.mulder@gmail.com>
2024-05-10 15:11:02 -0400

    Very basic API call

--- src/components/TestComponent2/TestComponent2.js
+++ src/components/TestComponent2/TestComponent2.js
@@ -1,12 +1,28 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import './TestComponent2.css';
 
-const TestComponent2 = () => (
-  <div className="TestComponent2" data-testid="TestComponent2">
-    TestComponent2 Component
-  </div>
-);
+
+const TestComponent2 = () => {
+  const [poke, setPokes] = useState([]);
+  useEffect(() => {
+     fetch('https://pokeapi.co/api/v2/pokemon/ditto')
+        .then((response) => response.json())
+        .then((data) => {
+           console.log(data);
+           setPokes(data);
+        })
+        .catch((err) => {
+           console.log(err.message);
+        });
+  }, []);
+
+  return(
+    <div className='TestComponent2'>{poke.name}, Base speed:{poke.stats[5].base_stat}</div>
+  );
+};
+
 
 TestComponent2.propTypes = {};
 
